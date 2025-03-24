1730536b63b4fb0f9b7dfaf3d9b479b9d3eeb9eb
Tyler Mielke <tmielke1@icloud.com>
2025-03-24 09:28:24 -0400

    Starting the php part now

--- Homework-4-Redo.php
+++ Homework-4-Redo.php
@@ -5,15 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Homework 4</title>
 </head>
+
 <body class="body">
 <div class = "title" >
 <h1> I Can Guess Your Favorite Coffee </h1>
 </div>
 
-
-<body>
-    <?php
-
-    ?>
-</body>
+<form action="results.php" method="post" class="Coffee-Quiz">
+<?php   
+?>
 </html>
