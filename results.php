ea9815b3d02e766ac79865586e998bfb2ffae8a8
Tyler Mielke <tmielke1@icloud.com>
2025-03-24 13:26:50 -0400

    Starting the php part

--- results.php
+++ results.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Homework 4 Realut</title>
+    <title>Homework 4 Results</title>
 </head>
 
 <body class="body">
@@ -11,3 +11,9 @@
 <h1> Is This Your Favorite Coffee? </h1>
 <br>
 </div>
+
+<?php
+
+?>
+</body>
+</html>
