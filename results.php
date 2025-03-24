6ae35910614584d12867c9f86028943503f1d1cf
Tyler Mielke <tmielke1@icloud.com>
2025-03-24 13:23:02 -0400

    Added the boilerplate

---
+++ results.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homework 4 Realut</title>
+</head>
+
+<body class="body">
+<div class = "title" >
+<h1> Is This Your Favorite Coffee? </h1>
+<br>
+</div>
